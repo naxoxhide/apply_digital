e7bb8fe19c30833d7cd90fe687b7c0eed2d8dcd0
Ignacio Rojas <irojas.kranio@bicevida.cl>
2025-02-09 23:32:45 -0300

    fix[dev]: fix gitignore and remove dist in the cache

--- dist/reports/reports.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReportsService } from './reports.service';
-export declare class ReportsController {
-    private readonly reportsService;
-    constructor(reportsService: ReportsService);
-    getDeletedPercentage(): Promise<string>;
-    getNonDeletedPercentage(hasPrice: string, startDate: string, endDate: string): Promise<string>;
-    getCustomReport(): Promise<any[]>;
-}
